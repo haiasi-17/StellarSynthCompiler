45d2ba9a18ecdd949fcb70fab67947a667af8655
Miyukiin <149550452+Miyukiin@users.noreply.github.com>
2024-05-03 15:22:54 +0800

    Added Remarks and ways to test Individual Files

--- Semantic.py
+++ Semantic.py
@@ -1,5 +1,6 @@
 import re
 import Resources
+import Lexer, Syntax
 
 class SemanticAnalyzer:
     def __init__(self, tokens):
@@ -21290,4 +21291,14 @@ class SemanticAnalyzer:
         else:
             self.errors.append(
                 f"(Line {self.line_number}) | Semantic Error: Variable not declared")
-
+            
+if __name__ == "__main__":
+    errors, tokens = Lexer.read_text('StellarSynth')
+    Semantic_analyzer = SemanticAnalyzer(tokens)
+    Semantic_analyzer.parse_top_program()
+    print(SemanticAnalyzer.errors)
+    
+'''
+Remarks:
+Some Bugs still.
+'''
